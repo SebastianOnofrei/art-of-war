49ec86279e8cca811d9189eddbba1e4d1999c046
Sebastian <bastianonofrei@gmail.com>
2023-06-27 17:28:38 +0300

    Added text for the 3 battles - awaiting to create a mechanism to show and hide the paragraphs of the battles

--- src/components/organisms/battles/Battles.jsx
+++ src/components/organisms/battles/Battles.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 import './Battles.scss'
 const Battles = () => {
   return (
-    <section className='battles' id='Battles'>Battles</section>
-  )
+		<section className="battles" id="Battles">
+			<div className="battles-text">
+				<h3>THE GENERAL WHO NEVER LOST A</h3>
+				<h1>BATTLE</h1>
+				{/* maybe implement here an atom */}
+				<ul className="battle-features">
+					<li className="battle-features__boju">
+						<h2>Battle of Boju</h2>
+						<p>
+							The Battle of Boju and the Wu-Chu War the first large-scale war of
+							the Eastern Zhou Dynasty.
+						</p>
+					</li>
+					<li className="battle-features__eight-princes">
+						<h2>War of the Eight Princes</h2>
+						<p>
+							Power struggle among sons of Emperor Wu in ancient China's Jin
+							Dynasty, leading to instability and downfall.
+						</p>
+					</li>
+					<li className="battle-features__red-cliffs">
+						<h2>Battle of Red Cliffs</h2>
+						<p>
+							Epic naval battle in ancient China, where allied forces defeated
+							Cao Cao's army, shaping history.
+						</p>
+					</li>
+				</ul>
+			</div>
+		</section>
+	);
 }
 
 export default Battles

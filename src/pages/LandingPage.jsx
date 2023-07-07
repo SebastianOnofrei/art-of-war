ed61c689e5fff22f16f3a7b03f6e084d51547ade
Sebastian <bastianonofrei@gmail.com>
2023-07-07 21:52:06 +0300

    Fixed about mobile :)

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,8 +10,8 @@ import Footer from "../components/organisms/footer/Footer";
 const LandingPage = () => {
   return (
 		<>
-			{/* <Hero />
-			<About /> */}
+			<Hero />
+			<About />
 			<Battles />
 			{/* <Battle_Requirements />
 			<Testimonials />
